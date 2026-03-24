94c7b380f8e1460abb33f6ec0330d8e2d036ab07
Javier Bradlim <144743434+jbradlim08@users.noreply.github.com>
2026-03-24 18:15:53 -0500

    Learn calling API

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ const jokeBtn = document.getElementById('joke-btn');
 const setup = document.getElementById('setup');
 const punchline = document.getElementById('punchline');
 
+
 // Add an event listener to the button
 jokeBtn.addEventListener('click', function() {
   console.log('Getting a joke...');
+  fetch('https://official-joke-api.appspot.com/random_joke')
+    .then(response => response.json())
+    .then(data => {
+      setup.textContent = data.setup;
+      punchline.textContent = data.punchline;
+    })
 });
